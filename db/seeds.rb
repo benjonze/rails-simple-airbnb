38988d158e1fd5c4bf0e738148b1737c935495cf
Ben Jones <benalexanderjones@gmail.com>
2023-11-15 16:24:45 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts 'Creating 10 fake flats...'
+100.times do
+  flat = Flat.new(
+    name: Faker::Hipster.sentence,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Hipster.paragraph,
+    price_per_night: rand(10..100),
+    number_of_guests: rand(1..12)
+  )
+  flat.save!
+end
+puts 'Finished!'
